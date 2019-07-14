d03c698bc8fd633dcdc4b60ae8de25699d091908
Petar Ivanković Milošević <pero.milosev@yahoo.com>
2019-07-14 13:28:50 +0200

    Scroll view works, keyboard hides on tap, still can't figure out how to center content

--- TVShows/TVShows/Login/LoginViewController.swift
+++ TVShows/TVShows/Login/LoginViewController.swift
@@ -14,6 +14,7 @@ final class LoginViewController: UIViewController {
     
     @IBOutlet private weak var rememberMeCheckboxButton: UIButton!
     @IBOutlet private weak var logInButton: UIButton!
+    @IBOutlet private weak var scrollView: UIScrollView!
     
     //MARK :- Properties
     
@@ -24,33 +25,61 @@ final class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureUI()
+        keyboardEventHappened()
     }
     
     private func configureUI() {
         logInButton.layer.cornerRadius = 10
+        //Set up a tap listener that dismisses the keyboard upon tapping outside text fields
+        let tap = UITapGestureRecognizer(
+            target: self.view,
+            action: #selector(UIView.endEditing(_:)))
+        tap.cancelsTouchesInView = false
+        self.view.addGestureRecognizer(tap)
     }
     
     //MARK :- Actions
     
     @IBAction private func checkboxStateChanged() {
         if rememberMeIsSelected { //I hope this is a good way of doing these
-            rememberMeCheckboxButton.setImage(UIImage(named: "ic-checkbox-empty"), for: UIControl.State.normal)
+            rememberMeCheckboxButton.setImage(
+                UIImage(named: "ic-checkbox-empty"),
+                for: UIControl.State.normal)
             rememberMeIsSelected = false
         }
         else{
-            rememberMeCheckboxButton.setImage(UIImage(named: "ic-checkbox-filled"), for: UIControl.State.normal)
+            rememberMeCheckboxButton.setImage(
+                UIImage(named: "ic-checkbox-filled"),
+                for: UIControl.State.normal)
             rememberMeIsSelected = true
         }
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func keyboardEventHappened() {
+        NotificationCenter
+            .default
+            .addObserver(
+                forName: UIResponder.keyboardWillShowNotification,
+                object: nil,
+                queue: .main
+            ) { [weak self] notification in
+                if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+                    let keyboardRectangle = keyboardFrame.cgRectValue
+                    let keyboardHeight = keyboardRectangle.height
+                    self?.scrollView.contentInset.bottom = keyboardHeight
+                }
+            }
+        NotificationCenter
+            .default
+            .addObserver(
+                forName: UIResponder.keyboardWillHideNotification,
+                object: nil,
+                queue: .main
+            ) { [weak self] notification in
+                // keyboard is about to hide, handle UIScrollView contentInset, e.g.
+                self?.scrollView.contentInset = .zero
+            }
+        
     }
-    */
 
 }
